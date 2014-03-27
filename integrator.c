1f4899745adda8c756b80241526f41532f97dab3
BigBo <bigbo@BigBo.(none)>
2014-03-28 01:34:06 +0400

    first commit

--- integrator.c
+++ integrator.c
@@ -3,24 +3,24 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-double f( double x) { 
+double func( double x) { 
 	return (x*x)+7; }
 
-double simple_integrate(double a, double b, double dx, double (*f)(double) ) {
-	double result=0;
-	for(double x=a; x<b; x+=dx)
-		result+=(f(x) + f(x+dx))/2*dx;
-	return result;
-}
-
-void mp_integrate(double a, double b, double dx, double (*f)(double) , int pn) {
+int main(int argc, char **argv) {
+	double a=atof(argv[1]);
+	double b=atof(argv[2]);
+	double dx=atof(argv[3]);
+	int pn=atoi(argv[4]);
+	
 	double delta=(b-a)/pn;
 	for(int i = 0; i<pn; ++i) {
 		int pid =fork();
 		if (pid==0) {
 			int fd=open("temp",O_WRONLY|O_CREAT|O_APPEND, 0666);
-			double res = simple_integrate(a+delta*i, a+delta*(i+1), dx, f);
-			write(fd, &res, sizeof(res));
+			double result=0;
+			for(double x=a+delta*i; x<a+delta*(i+1); x+=dx)
+				result+=(func(x) + func(x+dx))/2*dx;
+			write(fd, &result, sizeof (double));
 			close(fd);
 			exit(0);
 		}
@@ -34,9 +34,5 @@ void mp_integrate(double a, double b, double dx, double (*f)(double) , int pn) {
 			close(fd);
 			unlink("temp");
 			printf("%lf", res_total);
-}
-
-int main() {	
-	mp_integrate(0,10, 0.00000001, f, 10);
 	return 0;
 }
